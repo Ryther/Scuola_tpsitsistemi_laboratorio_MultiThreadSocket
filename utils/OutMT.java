b4065efca382befe1f5a59c772ea23b427077308
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-04-18 23:36:31 +0200

    Creata logica per la gestione dei messaggi di debug. Quando "activate" viene lanciato su un oggetto creato, i messaggi verranno visualizzati. Quando "deactivate" viene lanciato su un oggetto creato, i messaggi non verranno visualizzati.

--- utils/OutMT.java
+++ utils/OutMT.java
@@ -6,14 +6,28 @@ package utils;
  */
 public class OutMT {
     
+    private static boolean active = false;
+    
     // Display a message, preceded by
     // the name of the current thread
     public static void threadMessage(String message) {
-        String threadName =
-            Thread.currentThread().getName();
-        System.out.format("%s: %s%n",
-                          threadName,
-                          message);
+        
+        if (active) {
+            String threadName =
+                Thread.currentThread().getName();
+            System.out.format("%s: %s%n",
+                              threadName,
+                              message);
+        }
     }
     
+    public void activate() {
+        
+        this.active = true;
+    }
+    
+    public void deactivate() {
+        
+        this.active = false;
+    }
 }
