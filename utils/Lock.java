f72c5b559ec646414db2cf182ff06ac626306bf8
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-04-18 23:48:55 +0200

    Creata logica per la gestione dei lock.

---
+++ utils/Lock.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.util.concurrent.locks.ReentrantLock;
+import runner.Consts;
+
+public class Lock {
+	private static final ReentrantLock reLock = new ReentrantLock();
+	private static final ReentrantLock[] lock = new ReentrantLock[Consts.LOCKS];
+	
+	public static void acquire(int n)
+	{
+		reLock.lock();
+		if (lock[n]==null){
+			
+			lock[n] = new ReentrantLock();
+			//Utils.tO("Lock Created " + n);
+			
+		}
+		reLock.unlock();
+		
+		//Utils.tO("Lock PreAcquired " + n);
+		lock[n].lock();
+		//Utils.tO("Lock Acquired " + n);
+	}
+	
+	public static void release(int n)
+	{
+		lock[n].unlock();
+		//Utils.tO("Lock Relased " + n);
+	}
+}
