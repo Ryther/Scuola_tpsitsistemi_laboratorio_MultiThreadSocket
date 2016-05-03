a3391f0b23389a006f633392fc572dfa05f05437
WEBEVO_EZanoni <e.zanoni@webevolution.eu>
2016-05-03 17:48:41 +0200

    Modificato utils.Lock: aggiunta classe create, corrette logiche di uso

--- utils/Lock.java
+++ utils/Lock.java
@@ -3,48 +3,37 @@ package utils;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Lock {
-	private static final ReentrantLock reLock = new ReentrantLock();
-	private static final List<ReentrantLock> lock = new LinkedList<ReentrantLock>();
+    private static final ReentrantLock reLock = new ReentrantLock();
+    private static final List<ReentrantLock> lock = new LinkedList<ReentrantLock>();
+
+    public static void create(int n) {
+
+        reLock.lock();
+        if (n == lock.size()) {
+            
+            lock.add(new ReentrantLock());
+        } else if (n > lock.size()) {
+            
+            Logger.getLogger(Lock.class.getName()).log(Level.SEVERE, "Il numero utilizzato per il lock non è consecutivo al lock creato in precedenza, verificare la numerazione.");
+            throw new IndexOutOfBoundsException();
+        }
+        reLock.unlock();
+    }
+
+    public static void acquire(int n) {
+
+        lock.get(n).lock();
+    }
 	
-	public static void acquire(int n)
-	{
-		reLock.lock();
-		if (n >= lock.size()){
-			
-                    lock.add(n, new ReentrantLock());
-                    //Utils.tO("Lock Created " + n);
-		}
-                
-                if (lock.get(n) == null) {
-                    lock.set(n, new ReentrantLock());
-                    //Utils.tO("Lock Created " + n);
-                }
-		reLock.unlock();
-		
-		//Utils.tO("Lock PreAcquired " + n);
-		lock.get(n).lock();
-		//Utils.tO("Lock Acquired " + n);
-	}
-	
-	public static void release(int n)
-	{
-            
-            if (lock.get(n)!=null && lock.get(n).isLocked()){
-			
-		lock.get(n).unlock();
-                //Utils.tO("Lock Relased " + n);
-            } 
-            
-            if (lock.get(n) == null) {
-                
-                throw new NullPointerException();
-            }
-            
-            if (!lock.get(n).isLocked()) {
-                
-                throw new IllegalMonitorStateException();
-            }
-	}
+    public static void release(int n) {
+
+        if (lock.get(n)!=null && lock.get(n).isLocked()){
+
+            lock.get(n).unlock();
+        } 
+    }
 }
