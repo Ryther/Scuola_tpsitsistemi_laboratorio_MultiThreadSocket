bb8cc494b34bd99440fd5f33ebd1de577fb461dc
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-03 16:50:09 +0200

    Modificato utils.Lock: sostituito array fisso con LinkedList

--- utils/Lock.java
+++ utils/Lock.java
@@ -1,31 +1,50 @@
 package utils;
 
 import java.util.concurrent.locks.ReentrantLock;
-import runner.Consts;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Lock {
 	private static final ReentrantLock reLock = new ReentrantLock();
-	private static final ReentrantLock[] lock = new ReentrantLock[Consts.LOCKS];
+	private static final List<ReentrantLock> lock = new LinkedList<ReentrantLock>();
 	
 	public static void acquire(int n)
 	{
 		reLock.lock();
-		if (lock[n]==null){
+		if (n >= lock.size()){
 			
-			lock[n] = new ReentrantLock();
-			//Utils.tO("Lock Created " + n);
-			
+                    lock.add(n, new ReentrantLock());
+                    //Utils.tO("Lock Created " + n);
 		}
+                
+                if (lock.get(n) == null) {
+                    lock.set(n, new ReentrantLock());
+                    //Utils.tO("Lock Created " + n);
+                }
 		reLock.unlock();
 		
 		//Utils.tO("Lock PreAcquired " + n);
-		lock[n].lock();
+		lock.get(n).lock();
 		//Utils.tO("Lock Acquired " + n);
 	}
 	
 	public static void release(int n)
 	{
-		lock[n].unlock();
-		//Utils.tO("Lock Relased " + n);
+            
+            if (lock.get(n)!=null && lock.get(n).isLocked()){
+			
+		lock.get(n).unlock();
+                //Utils.tO("Lock Relased " + n);
+            } 
+            
+            if (lock.get(n) == null) {
+                
+                throw new NullPointerException();
+            }
+            
+            if (!lock.get(n).isLocked()) {
+                
+                throw new IllegalMonitorStateException();
+            }
 	}
 }
